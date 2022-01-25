037020834bcf01b3608afca929a3b55332e6b2b1
kw520 <1151094976@qq.com>
2022-01-25 16:31:06 +0800

    feat: 工作模块，滚动日志

--- center-core/src/main/java/com/itchenyang/CenterCoreApplication.java
+++ center-core/src/main/java/com/itchenyang/CenterCoreApplication.java
@@ -1,11 +1,16 @@
 package com.itchenyang;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CenterCoreApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(CenterCoreApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(CenterCoreApplication.class, args);
+        LOG.info("distribute center core application started!");
     }
 }
